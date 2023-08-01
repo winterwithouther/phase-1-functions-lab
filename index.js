eabcb8a29e316e5c7a144de54a0e3324c643f3fd
Eman Gurung <egurung1117@gmail.com>
2023-08-01 18:17:27 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let scubarHq = 42;
+    if (blocks >= scubarHq) {
+        return blocks - scubarHq;
+    } else {
+        return scubarHq - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    const distanceInBlocks = distanceFromHqInBlocks(blocks);
+    return distanceInBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const calculatedDistance = distanceTravelledInFeet(start, destination);
+    if (calculatedDistance >= 2500) {
+        return "cannot travel that far";
+    } else {
+        let price = 0;
+        if (calculatedDistance > 2000) {
+            price += 25;
+        } else if (calculatedDistance >= 400) {
+            price += (calculatedDistance - 400) * 0.02;
+        }
+        return price;
+    }
+}
